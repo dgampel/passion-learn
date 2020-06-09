b6e5133d6b6c0312d496fd4dcf90ec1b18b3cffd
Daphne Gampel <daphnegampel@gmail.com>
2020-06-09 08:13:52 -0700

    algo for searching with 0(logn) time complexity

--- examples/binary_search.py
+++ examples/binary_search.py
@@ -3,7 +3,7 @@ def binary_search(array,first,last,x):
     array.sort()
 
     if last >= 1:                   #base case
-        mid = 1 + (last - 1) // 2 
+        middle = 1 + (last - 1) // 2 
     
         if array[middle] == x:      #if element is present at middle
             return middle
@@ -14,9 +14,21 @@ def binary_search(array,first,last,x):
         else:                       #if element is in right half
             return binary_search(array,middle+1,last,x)
 
-    else:
+    else:                           #if element is not present in array
         return -1
 
-array = [1, 21, 14, 3, 2, 18, 10, 3, 47, 7,  
-x = 10
+array = [1, 21, 14, 3, 2, 18, 10, 3, 47, 7]  # test array
+x = 10                              #test element
+
+first = 0
+last = len(array)                   
+
+answer = binary_search(array,first,last,x)
+
+if answer != -1:
+    print("Element is present at index", str(answer))
+
+else:
+    print("Element is not present in array")
+   
 
